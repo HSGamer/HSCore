022541b811fecb16046d4c16c76f0a968279012d
Hasan Demirtaş <utsukushihito@outlook.com>
2020-08-27 15:20:02 +0300

    Update Paths.java

--- HSCore-bukkit/HSCore-bukkit-config/src/main/java/me/hsgamer/hscore/bukkit/config/Paths.java
+++ HSCore-bukkit/HSCore-bukkit-config/src/main/java/me/hsgamer/hscore/bukkit/config/Paths.java
@@ -7,30 +7,72 @@ public final class Paths {
   private Paths() {
   }
 
+  /**
+   * Create a config path
+   *
+   * @param path the path to the value
+   * @param def  the default value if it's not found
+   */
   public static BooleanConfigPath booleanPath(String path, boolean def) {
     return new BooleanConfigPath(path, def);
   }
 
+  /**
+   * Create a config path
+   *
+   * @param path the path to the value
+   * @param def  the default value if it's not found
+   */
   public static DoubleConfigPath doublePath(String path, Double def) {
     return new DoubleConfigPath(path, def);
   }
 
+  /**
+   * Create a config path
+   *
+   * @param path the path to the value
+   * @param def  the default value if it's not found
+   */
   public static FloatConfigPath floatPath(String path, Float def) {
     return new FloatConfigPath(path, def);
   }
 
+  /**
+   * Create a config path
+   *
+   * @param path the path to the value
+   * @param def  the default value if it's not found
+   */
   public static IntegerConfigPath integerPath(String path, Integer def) {
     return new IntegerConfigPath(path, def);
   }
 
+  /**
+   * Create a config path
+   *
+   * @param path the path to the value
+   * @param def  the default value if it's not found
+   */
   public static LongConfigPath longPath(String path, Long def) {
     return new LongConfigPath(path, def);
   }
 
+  /**
+   * Create a config path
+   *
+   * @param path the path to the value
+   * @param def  the default value if it's not found
+   */
   public static <T> SimpleConfigPath<T> simplePath(String path, T def) {
     return new SimpleConfigPath<>(path, def);
   }
 
+  /**
+   * Create a config path
+   *
+   * @param path the path to the value
+   * @param def  the default value if it's not found
+   */
   public static StringConfigPath stringPath(String path, String def) {
     return new StringConfigPath(path, def);
   }
