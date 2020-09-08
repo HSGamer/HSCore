43346f93756826ece0747e065c9a4b62bf581323
HSGamer <huynhqtienvtag@gmail.com>
2020-09-08 17:56:35 +0700

    add BaseConfigPath and AdvancedConfigPath

---
+++ HSCore-bukkit/HSCore-bukkit-config/src/main/java/me/hsgamer/hscore/bukkit/config/AdvancedConfigPath.java
@@ -0,0 +1,68 @@
+package me.hsgamer.hscore.bukkit.config;
+
+/**
+ * An advanced config path
+ *
+ * @param <F> the type of the raw value from the config
+ * @param <T> the type of the final value
+ */
+public abstract class AdvancedConfigPath<F, T> extends BaseConfigPath<T> {
+
+  /**
+   * Create a config path
+   *
+   * @param path the path to the value
+   * @param def  the default value if it's not found
+   */
+  public AdvancedConfigPath(String path, T def) {
+    super(path, def);
+  }
+
+  /**
+   * Get the raw value from the config
+   *
+   * @param pluginConfig the config
+   * @return the raw value
+   */
+  public abstract F getFromConfig(PluginConfig pluginConfig);
+
+  /**
+   * Convert to the final value
+   *
+   * @param rawValue the raw value
+   * @return the final value
+   */
+  public abstract T convert(F rawValue);
+
+  /**
+   * Convert to the raw value
+   *
+   * @param value the value
+   * @return the raw value
+   */
+  public abstract F convertToRaw(T value);
+
+  @Override
+  public T getValue() {
+    if (config == null) {
+      return def;
+    }
+
+    F rawValue = getFromConfig(config);
+    if (rawValue == null) {
+      return def;
+    }
+
+    T value = convert(rawValue);
+    return value != null ? value : def;
+  }
+
+  @Override
+  public void setValue(T value) {
+    if (config == null) {
+      return;
+    }
+
+    config.getConfig().set(path, convertToRaw(value));
+  }
+}
--- HSCore-bukkit/HSCore-bukkit-config/src/main/java/me/hsgamer/hscore/bukkit/config/ConfigPath.java
+++ HSCore-bukkit/HSCore-bukkit-config/src/main/java/me/hsgamer/hscore/bukkit/config/ConfigPath.java
@@ -7,12 +7,9 @@ import java.util.function.Function;
  *
  * @param <T> the type of the value
  */
-public class ConfigPath<T> {
+public abstract class ConfigPath<T> extends BaseConfigPath<T> {
 
-  protected final T def;
-  private final String path;
   private final Function<Object, T> typeConverter;
-  private PluginConfig config;
 
   /**
    * Create a config path
@@ -22,55 +19,16 @@ public class ConfigPath<T> {
    * @param typeConverter how to convert the raw object to the needed type of value
    */
   public ConfigPath(String path, T def, Function<Object, T> typeConverter) {
-    this.path = path;
-    this.def = def;
+    super(path, def);
     this.typeConverter = typeConverter;
   }
 
-  /**
-   * Set the config for this ConfigPath.
-   * <br>This only adds the default value to the config.
-   * <br>You will need to enable the copyDefault option and save the config manually.
-   *
-   * @param config the config
-   */
-  public void setConfig(PluginConfig config) {
-    this.config = config;
-    config.getConfig().addDefault(path, def);
-  }
-
-  /**
-   * Get the value
-   *
-   * @return the value
-   */
+  @Override
   public T getValue() {
     if (config == null) {
       return def;
     }
 
     return typeConverter.apply(config.get(path, def));
-  }
-
-  /**
-   * Set the value
-   *
-   * @param value the value
-   */
-  public void setValue(T value) {
-    if (config == null) {
-      return;
-    }
-
-    config.getConfig().set(path, value);
-  }
-
-  /**
-   * Get the path to the value
-   *
-   * @return the path
-   */
-  public String getPath() {
-    return path;
   }
 }
