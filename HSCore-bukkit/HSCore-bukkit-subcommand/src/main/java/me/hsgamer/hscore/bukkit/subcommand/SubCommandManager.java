fe29274843f26f32ef0be25940921ccb990d2c49
HSGamer <huynhqtienvtag@gmail.com>
2020-09-11 22:01:11 +0700

    changes in SubCommandManager
    use CaseInsensitiveStringMap
    add unregisterSubcommand
    add getSubcommands
    arguments will be copied properly for the sub-command

--- HSCore-bukkit/HSCore-bukkit-subcommand/src/main/java/me/hsgamer/hscore/bukkit/subcommand/SubCommandManager.java
+++ HSCore-bukkit/HSCore-bukkit-subcommand/src/main/java/me/hsgamer/hscore/bukkit/subcommand/SubCommandManager.java
@@ -3,11 +3,11 @@ package me.hsgamer.hscore.bukkit.subcommand;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 import me.hsgamer.hscore.bukkit.utils.MessageUtils;
+import me.hsgamer.hscore.map.CaseInsensitiveStringMap;
 import org.bukkit.command.CommandSender;
 
 /**
@@ -17,7 +17,7 @@ public class SubCommandManager {
 
   private static final String HELP = "help";
 
-  protected final Map<String, SubCommand> subcommands = new HashMap<>();
+  protected final Map<String, SubCommand> subcommands = new CaseInsensitiveStringMap<>();
   protected Supplier<List<String>> helpHeader = Collections::emptyList;
   protected Supplier<List<String>> helpInfo = () -> Arrays.asList(
       "&e/<label> <subcommand>",
@@ -53,7 +53,8 @@ public class SubCommandManager {
       list.forEach(s -> MessageUtils.sendMessage(sender, s));
       return true;
     } else {
-      return subcommands.get(args[0]).onCommand(sender, label, args);
+      return subcommands.get(args[0])
+          .onCommand(sender, label, Arrays.copyOfRange(args, 1, args.length));
     }
   }
 
@@ -64,6 +65,33 @@ public class SubCommandManager {
    */
   public void registerSubcommand(SubCommand subCommand) {
     subcommands.put(subCommand.getName(), subCommand);
+  }
+
+  /**
+   * Unregister a sub-command
+   *
+   * @param name the name of the sub-command
+   */
+  public void unregisterSubcommand(String name) {
+    subcommands.remove(name);
+  }
+
+  /**
+   * Unregister a sub-command
+   *
+   * @param subCommand the sub-command
+   */
+  public void unregisterSubcommand(SubCommand subCommand) {
+    subcommands.remove(subCommand.getName());
+  }
+
+  /**
+   * Get the available sub-commands
+   *
+   * @return the unmodifiable map of sub-commands
+   */
+  public Map<String, SubCommand> getSubcommands() {
+    return Collections.unmodifiableMap(subcommands);
   }
 
   /**
@@ -80,14 +108,17 @@ public class SubCommandManager {
       list.add(HELP);
       list.addAll(subcommands.keySet());
     } else if (subcommands.containsKey(args[0])) {
-      list = subcommands.get(args[0]).onTabComplete(sender, label, args);
-    } else if (HELP.startsWith(args[0])) {
-      list.add(HELP);
+      list = subcommands.get(args[0])
+          .onTabComplete(sender, label, Arrays.copyOfRange(args, 1, args.length));
     } else {
       for (String subcommand : subcommands.keySet()) {
         if (subcommand.startsWith(args[0])) {
           list.add(subcommand);
         }
+      }
+
+      if (HELP.startsWith(args[0])) {
+        list.add(HELP);
       }
     }
     return list;
