929e5695f4877d7447269417f8223eeea2bfb3be
Hasan Demirtaş <utsukushihito@outlook.com>
2020-09-19 20:13:35 +0300

    Create ConfigLoader.java

---
+++ HSCore-config/src/main/java/me/hsgamer/hscore/config/ConfigLoader.java
@@ -0,0 +1,36 @@
+package me.hsgamer.hscore.config;
+
+import java.lang.reflect.Field;
+
+/**
+ * A config loader class to load config paths
+ *
+ * Usage example:
+ * <pre>
+ *   
+ * </pre>
+ */
+public final class ConfigLoader {
+
+  private ConfigLoader() {
+  }
+
+  /**
+   * Load the config path that's in the config instance
+   *
+   * @param config config instance to load
+   * @param <C>    config instance's class type
+   */
+  public static <C extends Config> void load(C config) {
+    final Field[] fields = config.getClass().getDeclaredFields();
+    for (Field field : fields) {
+      if (BaseConfigPath.class.isAssignableFrom(field.getType())) {
+        try {
+          ((BaseConfigPath<?>) field.get(config)).setConfig(config);
+        } catch (IllegalAccessException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+}
