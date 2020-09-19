d5b5d6141069bfec458be8d76a6720a78eecc0bf
Hasan Demirtaş <utsukushihito@outlook.com>
2020-09-19 13:50:09 +0300

    new Itemstack, PotionEffect, FireworkEffect advanced paths. (#6)
    * Create ItemStackConfigPath.java
    
    * potion effect
    
    * Update PotionEffectConfigPath.java
    
    * firework effect

---
+++ HSCore-bukkit/HSCore-bukkit-config-extra/src/main/java/me/hsgamer/hscore/bukkit/config/path/FireworkEffectConfigPath.java
@@ -0,0 +1,36 @@
+package me.hsgamer.hscore.bukkit.config.path;
+
+import me.hsgamer.hscore.bukkit.config.AdvancedConfigPath;
+import me.hsgamer.hscore.bukkit.config.PluginConfig;
+import org.bukkit.FireworkEffect;
+import org.bukkit.potion.PotionEffect;
+
+import java.util.Map;
+
+public class FireworkEffectConfigPath extends AdvancedConfigPath<Map<String, Object>, FireworkEffect> {
+
+  /**
+   * Create a config path
+   *
+   * @param path the path to the value
+   * @param def  the default value if it's not found
+   */
+  public FireworkEffectConfigPath(String path, FireworkEffect def) {
+    super(path, def);
+  }
+
+  @Override
+  public Map<String, Object> getFromConfig(PluginConfig pluginConfig) {
+    return pluginConfig.getConfig().getConfigurationSection(path).getValues(false);
+  }
+
+  @Override
+  public FireworkEffect convert(Map<String, Object> rawValue) {
+    return ((FireworkEffect) FireworkEffect.deserialize(rawValue));
+  }
+
+  @Override
+  public Map<String, Object> convertToRaw(FireworkEffect value) {
+    return value.serialize();
+  }
+}
---
+++ HSCore-bukkit/HSCore-bukkit-config-extra/src/main/java/me/hsgamer/hscore/bukkit/config/path/PotionEffectConfigPath.java
@@ -0,0 +1,36 @@
+package me.hsgamer.hscore.bukkit.config.path;
+
+import me.hsgamer.hscore.bukkit.config.AdvancedConfigPath;
+import me.hsgamer.hscore.bukkit.config.PluginConfig;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+
+import java.util.Map;
+
+public class PotionEffectConfigPath extends AdvancedConfigPath<Map<String, Object>, PotionEffect> {
+
+  /**
+   * Create a config path
+   *
+   * @param path the path to the value
+   * @param def  the default value if it's not found
+   */
+  public PotionEffectConfigPath(String path, PotionEffect def) {
+    super(path, def);
+  }
+
+  @Override
+  public Map<String, Object> getFromConfig(PluginConfig pluginConfig) {
+    return pluginConfig.getConfig().getConfigurationSection(path).getValues(false);
+  }
+
+  @Override
+  public PotionEffect convert(Map<String, Object> rawValue) {
+    return new PotionEffect(rawValue);
+  }
+
+  @Override
+  public Map<String, Object> convertToRaw(PotionEffect value) {
+    return value.serialize();
+  }
+}
