c014a90c2df3f09c71d5fa4a5384861be060ba28
Hasan Demirtaş <utsukushihito@outlook.com>
2020-09-19 20:19:01 +0300

    ConfigLoader (#21)
    * Create ConfigLoader.java
    
    * Update ConfigLoader.java
    
    * Update ConfigLoader.java
    
    * Update ConfigLoader.java

---
+++ HSCore-config/src/main/java/me/hsgamer/hscore/config/ConfigLoader.java
@@ -0,0 +1,45 @@
+package me.hsgamer.hscore.config;
+
+import java.lang.reflect.Field;
+
+/**
+ * A class that loads config paths on the config instance
+ *
+ * Usage example:
+ * <pre>
+ *   public final class TestPlugin extends JavaPlugin {
+ *
+ *     &#64;Override
+ *     public void onEnable() {
+ *       final TestConfig testConfig = new TestConfig(this);
+ *       ConfigLoader.load(testConfig);
+ *       final boolean testBoolean = testConfig.testBoolean.getValue();
+ *     }
+ *
+ *   }
+ * </pre>
+ */
+public final class ConfigLoader {
+
+  private ConfigLoader() {
+  }
+
+  /**
+   * Load the config path that's in the config instance
+   *
+   * @param config config instance to load
+   * @param <C>    config instance's class type
+   */
+  public static <C extends Config> void load(C config) {
+    final Field[] fields = config.getClass().getDeclaredFields();
+    for (Field field : fields) {
+      if (BaseConfigPath.class.isAssignableFrom(field.getType())) {
+        try {
+          ((BaseConfigPath<?>) field.get(config)).setConfig(config);
+        } catch (IllegalAccessException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+}
