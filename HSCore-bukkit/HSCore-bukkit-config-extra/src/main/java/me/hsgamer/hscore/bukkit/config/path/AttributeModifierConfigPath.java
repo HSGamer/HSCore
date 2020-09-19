6696f2e871b2ffc874b0b04be747b986c58f0731
Hasan Demirtaş <utsukushihito@outlook.com>
2020-09-19 19:21:37 +0300

    More path type. (#17)

---
+++ HSCore-bukkit/HSCore-bukkit-config-extra/src/main/java/me/hsgamer/hscore/bukkit/config/path/AttributeModifierConfigPath.java
@@ -0,0 +1,30 @@
+package me.hsgamer.hscore.bukkit.config.path;
+
+import me.hsgamer.hscore.config.SerializableMapConfigPath;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.block.banner.Pattern;
+
+import java.util.Map;
+
+public class AttributeModifierConfigPath extends SerializableMapConfigPath<AttributeModifier> {
+
+  /**
+   * Create a config path
+   *
+   * @param path the path to the value
+   * @param def  the default value if it's not found
+   */
+  public AttributeModifierConfigPath(String path, AttributeModifier def) {
+    super(path, def);
+  }
+
+  @Override
+  public AttributeModifier convert(Map<String, Object> rawValue) {
+    return AttributeModifier.deserialize(rawValue);
+  }
+
+  @Override
+  public Map<String, Object> convertToRaw(AttributeModifier value) {
+    return value.serialize();
+  }
+}
