2ce9b849c06969e634de5c1d1eea11eccaaaa652
HSGamer <huynhqtienvtag@gmail.com>
2020-10-03 22:28:17 +0700

    updateVersion.sh

--- updateVersion.sh
+++ updateVersion.sh
@@ -4,3 +4,5 @@ cd ./HSCore-bukkit || return
 mvn -N versions:update-child-modules -DgenerateBackupPoms=false
 cd ../HSCore-config || return
 mvn -N versions:update-child-modules -DgenerateBackupPoms=false
+cd ../HSCore-sql || return
+mvn -N versions:update-child-modules -DgenerateBackupPoms=false
