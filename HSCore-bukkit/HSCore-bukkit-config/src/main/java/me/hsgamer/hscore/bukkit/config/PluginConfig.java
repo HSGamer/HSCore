7f7921f7c8771e4015acc03dc5f10788743b00cf
HSGamer <huynhqtienvtag@gmail.com>
2020-10-30 20:41:05 +0700

    update HSCore-bukkit-config to use Simple-Bukkit-YAML
    close #62

--- HSCore-bukkit/HSCore-bukkit-config/src/main/java/me/hsgamer/hscore/bukkit/config/PluginConfig.java
+++ HSCore-bukkit/HSCore-bukkit-config/src/main/java/me/hsgamer/hscore/bukkit/config/PluginConfig.java
@@ -1,21 +1,32 @@
 package me.hsgamer.hscore.bukkit.config;
 
-import me.hsgamer.hscore.bukkit.config.file.BukkitYamlConfiguration;
 import me.hsgamer.hscore.config.BaseConfig;
+import me.hsgamer.simplebukkityaml.configuration.file.YamlFile;
 import org.jetbrains.annotations.NotNull;
+import org.simpleyaml.exceptions.InvalidConfigurationException;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
 
 /**
  * A {@link me.hsgamer.hscore.config.Config} for Bukkit
  */
-public class PluginConfig extends BaseConfig<BukkitYamlConfiguration> {
+public class PluginConfig extends BaseConfig<YamlFile> {
   /**
    * Create a config with a provider
    *
    * @param file the config file
    */
   public PluginConfig(@NotNull File file) {
-    super(file, BukkitYamlConfiguration::loadConfiguration);
+    super(file, file1 -> {
+      final YamlFile yamlFile = new YamlFile(file1);
+      try {
+        yamlFile.loadWithComments();
+      } catch (InvalidConfigurationException | IOException e) {
+        LOGGER.log(Level.WARNING, "Error when loading yaml file", e);
+      }
+      return yamlFile;
+    });
   }
 }
