06a8d570607de13aee67d2cb5e85974b0c12c90c
Hasan Demirtaş <utsukushihito@outlook.com>
2020-11-01 02:38:33 +0300

    Create StickySupplier.java (#64)
    * Create StickySupplier.java
    
    * Update StickySupplier.java

---
+++ HSCore-common/src/main/java/me/hsgamer/hscore/common/StickySupplier.java
@@ -0,0 +1,53 @@
+package me.hsgamer.hscore.common;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+/**
+ * a class that uses {@link Supplier} and caches it to use after.
+ *
+ * @param <T> the value's type.
+ */
+public final class StickySupplier<T> implements Supplier<T> {
+
+  /**
+   * the original {@link Supplier}.
+   */
+  @NotNull
+  private final Supplier<T> origin;
+
+  /**
+   * the cache value.
+   */
+  @Nullable
+  private T cache;
+
+  /**
+   * ctor.
+   *
+   * @param origin the original {@link Supplier}.
+   */
+  public StickySupplier(@NotNull final Supplier<T> origin) {
+    this.origin = origin;
+  }
+
+  /**
+   * ctor.
+   *
+   * @param origin the original {@link T}.
+   */
+  public StickySupplier(@NotNull final T origin) {
+    this(() -> origin);
+  }
+
+  @Override
+  public T get() {
+    return Optional.ofNullable(this.cache).orElseGet(() -> {
+      this.cache = this.origin.get();
+      return this.cache;
+    });
+  }
+}
