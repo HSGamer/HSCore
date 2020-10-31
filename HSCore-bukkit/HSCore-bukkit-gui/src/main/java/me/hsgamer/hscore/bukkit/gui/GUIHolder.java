32d54a59e259a6b43c0dbd6dea6c25efe14416d0
HSGamer <huynhqtienvtag@gmail.com>
2020-10-31 17:38:13 +0700

    some methods can be in BaseHolder

--- HSCore-bukkit/HSCore-bukkit-gui/src/main/java/me/hsgamer/hscore/bukkit/gui/GUIHolder.java
+++ HSCore-bukkit/HSCore-bukkit-gui/src/main/java/me/hsgamer/hscore/bukkit/gui/GUIHolder.java
@@ -2,33 +2,13 @@ package me.hsgamer.hscore.bukkit.gui;
 
 import me.hsgamer.hscore.ui.BaseHolder;
 
-import java.util.HashMap;
-import java.util.Optional;
 import java.util.UUID;
 
 // TODO: Add items, actions and event handling
 public class GUIHolder extends BaseHolder<GUIDisplay> {
-  private final HashMap<UUID, GUIDisplay> displayMap = new HashMap<>();
 
   @Override
-  public GUIDisplay createDisplay(UUID uuid) {
-    GUIDisplay display = new GUIDisplay(uuid, this);
-    displayMap.put(uuid, display);
-    return display;
-  }
-
-  @Override
-  public void removeDisplay(UUID uuid) {
-    Optional.ofNullable(displayMap.remove(uuid)).ifPresent(GUIDisplay::close);
-  }
-
-  @Override
-  public Optional<GUIDisplay> getDisplay(UUID uuid) {
-    return Optional.ofNullable(displayMap.get(uuid));
-  }
-
-  @Override
-  public void updateAll() {
-    displayMap.values().forEach(GUIDisplay::update);
+  protected GUIDisplay newDisplay(UUID uuid) {
+    return new GUIDisplay(uuid, this);
   }
 }
--- HSCore-ui/HSCore-ui-common/src/main/java/me/hsgamer/hscore/ui/BaseHolder.java
+++ HSCore-ui/HSCore-ui-common/src/main/java/me/hsgamer/hscore/ui/BaseHolder.java
@@ -5,6 +5,38 @@ import java.util.function.Consumer;
 
 public abstract class BaseHolder<D extends Display> implements Holder<D> {
   private final Map<Class<?>, List<Consumer<Object>>> classListMap = new HashMap<>();
+  private final HashMap<UUID, D> displayMap = new HashMap<>();
+
+  /**
+   * Make a new display
+   *
+   * @param uuid the unique id
+   *
+   * @return the display
+   */
+  protected abstract D newDisplay(UUID uuid);
+
+  @Override
+  public D createDisplay(UUID uuid) {
+    D display = newDisplay(uuid);
+    displayMap.put(uuid, display);
+    return display;
+  }
+
+  @Override
+  public void removeDisplay(UUID uuid) {
+    Optional.ofNullable(displayMap.remove(uuid)).ifPresent(D::close);
+  }
+
+  @Override
+  public Optional<D> getDisplay(UUID uuid) {
+    return Optional.ofNullable(displayMap.get(uuid));
+  }
+
+  @Override
+  public void updateAll() {
+    displayMap.values().forEach(D::update);
+  }
 
   @Override
   public <T> void addEventConsumer(Class<T> eventClass, Consumer<T> eventConsumer) {
