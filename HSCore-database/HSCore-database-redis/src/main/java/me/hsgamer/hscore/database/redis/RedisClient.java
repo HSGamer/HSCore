8b0298fecc75659af77d0ed2a72a03bc39897cf4
HSGamer <huynhqtienvtag@gmail.com>
2020-11-04 18:47:04 +0700

    add RedisClient

---
+++ HSCore-database/HSCore-database-redis/src/main/java/me/hsgamer/hscore/database/redis/RedisClient.java
@@ -0,0 +1,56 @@
+package me.hsgamer.hscore.database.redis;
+
+import me.hsgamer.hscore.database.Client;
+import me.hsgamer.hscore.database.Setting;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * The client with Redis
+ */
+public class RedisClient implements Client<JedisPool> {
+
+  private final Setting setting;
+  private final JedisPool jedisPool;
+
+  /**
+   * Create new client
+   *
+   * @param setting the setting
+   */
+  public RedisClient(Setting setting) {
+    this.setting = setting;
+    this.jedisPool = new JedisPool(applyConfig(new JedisPoolConfig()), setting.getHost(), Integer.parseInt(setting.getPort()), 45000, setting.getPassword());
+  }
+
+  /**
+   * Apply pool config
+   *
+   * @param originalConfig the original pool config
+   *
+   * @return the applied pool config
+   */
+  protected JedisPoolConfig applyConfig(JedisPoolConfig originalConfig) {
+    return originalConfig;
+  }
+
+  @Override
+  public JedisPool getOriginal() {
+    return jedisPool;
+  }
+
+  /**
+   * Get the resource
+   *
+   * @return the resource
+   */
+  public Jedis getResource() {
+    return jedisPool.getResource();
+  }
+
+  @Override
+  public Setting getSetting() {
+    return setting;
+  }
+}
