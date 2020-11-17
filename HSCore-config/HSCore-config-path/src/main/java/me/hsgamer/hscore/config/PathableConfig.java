3956a5bafbdd821b31c699ae31764537cfa00e53
hsgamer <huynhqtienvtag@gmail.com>
2020-11-17 23:23:26 +0700

    PathableConfig as a wrapper

--- HSCore-config/HSCore-config-path/src/main/java/me/hsgamer/hscore/config/PathableConfig.java
+++ HSCore-config/HSCore-config-path/src/main/java/me/hsgamer/hscore/config/PathableConfig.java
@@ -1,25 +1,48 @@
 package me.hsgamer.hscore.config;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.simpleyaml.configuration.file.FileConfiguration;
 
-import java.io.File;
-
 /**
- * A {@link BaseConfig} implementation to load {@link ConfigPath} automatically
- *
- * @param <F> File configuration class type
+ * A {@link Config} implementation to load {@link ConfigPath} automatically
  */
-public class PathableConfig<F extends FileConfiguration> extends BaseConfig<F> {
+public class PathableConfig implements Config {
+  private final Config config;
 
   /**
-   * Create a config with a provider
+   * Create a pathable config
    *
-   * @param file     the config file
-   * @param provider the provider
+   * @param config the original config
    */
-  public PathableConfig(@NotNull final File file, @NotNull final ConfigProvider<F> provider) {
-    super(file, provider);
+  public PathableConfig(@NotNull final Config config) {
+    this.config = config;
+    setupConfig();
     PathLoader.loadPath(this);
+  }
+
+  @Override
+  public void setupConfig() {
+    config.setupConfig();
+  }
+
+  @Override
+  public void reloadConfig() {
+    config.reloadConfig();
+  }
+
+  @Override
+  public void saveConfig() {
+    config.saveConfig();
+  }
+
+  @Override
+  public @NotNull FileConfiguration getConfig() {
+    return config.getConfig();
+  }
+
+  @Override
+  public @Nullable Object get(@NotNull String path, @Nullable Object def) {
+    return config.get(path, def);
   }
 }
