338f027428d69b456b86d44673634f35645cb504
hsgamer <huynhqtienvtag@gmail.com>
2020-11-17 10:29:36 +0700

    viewPredicate on PredicateButton

--- HSCore-bukkit/HSCore-bukkit-gui/src/main/java/me/hsgamer/hscore/bukkit/gui/button/PredicateButton.java
+++ HSCore-bukkit/HSCore-bukkit-gui/src/main/java/me/hsgamer/hscore/bukkit/gui/button/PredicateButton.java
@@ -6,6 +6,7 @@ import org.bukkit.inventory.ItemStack;
 
 import java.util.UUID;
 import java.util.function.BiPredicate;
+import java.util.function.Predicate;
 
 /**
  * The button with click predicate
@@ -13,27 +14,43 @@ import java.util.function.BiPredicate;
 public class PredicateButton implements Button {
 
   private final Button button;
-  private final BiPredicate<UUID, InventoryClickEvent> predicate;
+  private final Predicate<UUID> viewPredicate;
+  private final BiPredicate<UUID, InventoryClickEvent> clickPredicate;
 
   /**
    * Create a predicate button
    *
-   * @param button    the original button
-   * @param predicate the click predicate
+   * @param button         the original button
+   * @param viewPredicate  the view predicate
+   * @param clickPredicate the click predicate
    */
-  public PredicateButton(Button button, BiPredicate<UUID, InventoryClickEvent> predicate) {
+  public PredicateButton(Button button, Predicate<UUID> viewPredicate, BiPredicate<UUID, InventoryClickEvent> clickPredicate) {
     this.button = button;
-    this.predicate = predicate;
+    this.viewPredicate = viewPredicate;
+    this.clickPredicate = clickPredicate;
+  }
+
+  /**
+   * Create a predicate button
+   *
+   * @param button         the original button
+   * @param clickPredicate the click predicate
+   */
+  public PredicateButton(Button button, BiPredicate<UUID, InventoryClickEvent> clickPredicate) {
+    this(button, uuid -> true, clickPredicate);
   }
 
   @Override
   public ItemStack getItemStack(UUID uuid) {
-    return button.getItemStack(uuid);
+    if (viewPredicate.test(uuid)) {
+      return button.getItemStack(uuid);
+    }
+    return null;
   }
 
   @Override
   public void handleAction(UUID uuid, InventoryClickEvent event) {
-    if (predicate.test(uuid, event)) {
+    if (viewPredicate.test(uuid) && clickPredicate.test(uuid, event)) {
       button.handleAction(uuid, event);
     }
   }
