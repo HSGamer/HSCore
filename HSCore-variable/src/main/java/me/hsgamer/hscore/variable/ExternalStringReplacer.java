bc7edbc80ccab308207259aef179a2652a383654
hsgamer <huynhqtienvtag@gmail.com>
2020-12-01 22:04:14 +0700

    docs

--- HSCore-variable/src/main/java/me/hsgamer/hscore/variable/ExternalStringReplacer.java
+++ HSCore-variable/src/main/java/me/hsgamer/hscore/variable/ExternalStringReplacer.java
@@ -2,7 +2,18 @@ package me.hsgamer.hscore.variable;
 
 import me.hsgamer.hscore.common.interfaces.StringReplacer;
 
+/**
+ * An external string replacer used in {@link VariableManager}
+ */
 public interface ExternalStringReplacer extends StringReplacer {
+
+  /**
+   * Check if the string can be replaced
+   *
+   * @param string the string
+   *
+   * @return true if it can
+   */
   default boolean canBeReplaced(String string) {
     return false;
   }
