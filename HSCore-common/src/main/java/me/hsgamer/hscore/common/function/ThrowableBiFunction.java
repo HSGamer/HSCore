f31960b2dbd5b63ecfceeddf770947f885cd7969
hsgamer <huynhqtienvtag@gmail.com>
2021-02-25 07:37:24 +0700

    add ThrowableBiFunction and ThrowableFunction

---
+++ HSCore-common/src/main/java/me/hsgamer/hscore/common/function/ThrowableBiFunction.java
@@ -0,0 +1,33 @@
+package me.hsgamer.hscore.common.function;
+
+import java.util.function.BiFunction;
+
+/**
+ * {@link BiFunction} but returns null if there is an exception
+ *
+ * @param <T> the type of the first argument to the function
+ * @param <U> the type of the second argument to the function
+ * @param <R> the type of the result of the function
+ */
+public interface ThrowableBiFunction<T, U, R> extends BiFunction<T, U, R> {
+  @Override
+  default R apply(T t, U u) {
+    try {
+      return applySafe(t, u);
+    } catch (Throwable throwable) {
+      return null;
+    }
+  }
+
+  /**
+   * Apply with throwable
+   *
+   * @param t the first value
+   * @param u the second value
+   *
+   * @return the result value
+   *
+   * @throws Throwable the exception
+   */
+  R applySafe(T t, U u) throws Throwable;
+}
---
+++ HSCore-common/src/main/java/me/hsgamer/hscore/common/function/ThrowableFunction.java
@@ -0,0 +1,31 @@
+package me.hsgamer.hscore.common.function;
+
+import java.util.function.Function;
+
+/**
+ * {@link Function} but returns null if there is an exception
+ *
+ * @param <T> the type of the input to the function
+ * @param <R> the type of the result of the function
+ */
+public interface ThrowableFunction<T, R> extends Function<T, R> {
+  @Override
+  default R apply(T t) {
+    try {
+      return applySafe(t);
+    } catch (Throwable throwable) {
+      return null;
+    }
+  }
+
+  /**
+   * Apply with throwable
+   *
+   * @param t the input
+   *
+   * @return the result
+   *
+   * @throws Throwable the exception
+   */
+  R applySafe(T t) throws Throwable;
+}
