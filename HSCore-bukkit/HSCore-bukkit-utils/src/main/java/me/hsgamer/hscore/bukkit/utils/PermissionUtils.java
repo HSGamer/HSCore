03cf047f13bfe1562bc44d8983b5f2d63c88e062
hsgamer <huynhqtienvtag@gmail.com>
2021-03-09 19:32:57 +0700

    add getNumbersFromPermissions

--- HSCore-bukkit/HSCore-bukkit-utils/src/main/java/me/hsgamer/hscore/bukkit/utils/PermissionUtils.java
+++ HSCore-bukkit/HSCore-bukkit-utils/src/main/java/me/hsgamer/hscore/bukkit/utils/PermissionUtils.java
@@ -1,10 +1,14 @@
 package me.hsgamer.hscore.bukkit.utils;
 
 import org.bukkit.Bukkit;
+import org.bukkit.permissions.Permissible;
 import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionAttachmentInfo;
 import org.bukkit.permissions.PermissionDefault;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.stream.Stream;
 
 /**
  * Methods on Permissions
@@ -30,5 +34,28 @@ public class PermissionUtils {
     Permission permission = new Permission(name, description, permissionDefault);
     Bukkit.getServer().getPluginManager().addPermission(permission);
     return permission;
+  }
+
+  /**
+   * Get the numbers from the permissions the permissible object has ([prefix].[number])
+   *
+   * @param permissible the permissible object
+   * @param prefix      the permission prefix
+   *
+   * @return the stream of numbers
+   */
+  @NotNull
+  public static Stream<Integer> getNumbersFromPermissions(Permissible permissible, String prefix) {
+    return permissible.getEffectivePermissions().stream()
+      .map(PermissionAttachmentInfo::getPermission)
+      .filter(permission -> permission.startsWith(prefix))
+      .map(permission -> permission.substring(prefix.length() + 1))
+      .flatMap(rawNumber -> {
+        try {
+          return Stream.of(Integer.parseInt(rawNumber));
+        } catch (Exception exception) {
+          return Stream.empty();
+        }
+      });
   }
 }
