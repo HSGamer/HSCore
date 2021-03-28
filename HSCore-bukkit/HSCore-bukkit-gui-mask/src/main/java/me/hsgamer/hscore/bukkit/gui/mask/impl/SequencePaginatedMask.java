67b92b65625b07190f53b8a08f3f464e84f1c9ed
hsgamer <huynhqtienvtag@gmail.com>
2021-03-28 14:37:02 +0700

    add SequencePaginatedMask

---
+++ HSCore-bukkit/HSCore-bukkit-gui-mask/src/main/java/me/hsgamer/hscore/bukkit/gui/mask/impl/SequencePaginatedMask.java
@@ -0,0 +1,89 @@
+package me.hsgamer.hscore.bukkit.gui.mask.impl;
+
+import me.hsgamer.hscore.bukkit.gui.button.Button;
+
+import java.util.*;
+
+/**
+ * A button paginated mask, where each {@link Button} is a page
+ */
+public class SequencePaginatedMask extends PaginatedMask {
+  protected final List<Integer> slots = new ArrayList<>();
+  protected final List<Button> buttons = new ArrayList<>();
+
+  /**
+   * Create a new mask
+   *
+   * @param name    the name of the mask
+   * @param slots   the slots
+   * @param buttons the buttons
+   */
+  public SequencePaginatedMask(String name, List<Integer> slots, Button... buttons) {
+    super(name);
+    this.slots.addAll(slots);
+    this.addButtons(buttons);
+  }
+
+  /**
+   * Add buttons to the mask
+   *
+   * @param buttons the buttons
+   */
+  public void addButtons(Button... buttons) {
+    this.addButtons(Arrays.asList(buttons));
+  }
+
+  /**
+   * Add buttons to the mask
+   *
+   * @param buttons the buttons
+   */
+  public void addButtons(List<Button> buttons) {
+    this.buttons.addAll(buttons);
+  }
+
+  /**
+   * Get the slots
+   *
+   * @return the slots
+   */
+  public List<Integer> getSlots() {
+    return Collections.unmodifiableList(slots);
+  }
+
+  /**
+   * Get the buttons
+   *
+   * @return the buttons
+   */
+  public List<Button> getButtons() {
+    return Collections.unmodifiableList(buttons);
+  }
+
+  @Override
+  public Map<Integer, Button> generateButtons(UUID uuid) {
+    Map<Integer, Button> map = new HashMap<>();
+    int basePage = this.getPage(uuid);
+    for (int i = 0; i < this.slots.size(); i++) {
+      map.put(this.slots.get(i), this.buttons.get(getExactPage(basePage + i)));
+    }
+    return map;
+  }
+
+  @Override
+  protected int getPageAmount() {
+    return this.buttons.size();
+  }
+
+  @Override
+  public void init() {
+    this.buttons.forEach(Button::init);
+  }
+
+  @Override
+  public void stop() {
+    this.buttons.forEach(Button::stop);
+    this.slots.clear();
+    this.pageNumberMap.clear();
+  }
+}
