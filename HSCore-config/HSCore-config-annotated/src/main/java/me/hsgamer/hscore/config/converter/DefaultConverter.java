184a9cca37c3d4c718395f943881a5fa06eb7509
Huynh Tien <huynhqtienvtag@gmail.com>
2021-08-17 17:23:00 +0700

    doc for this

--- HSCore-config/HSCore-config-annotated/src/main/java/me/hsgamer/hscore/config/converter/DefaultConverter.java
+++ HSCore-config/HSCore-config-annotated/src/main/java/me/hsgamer/hscore/config/converter/DefaultConverter.java
@@ -1,5 +1,8 @@
 package me.hsgamer.hscore.config.converter;
 
+/**
+ * The default {@link Converter}, which does nothing, so the raw value and the final value are the same type
+ */
 public class DefaultConverter implements Converter {
 
   @Override
