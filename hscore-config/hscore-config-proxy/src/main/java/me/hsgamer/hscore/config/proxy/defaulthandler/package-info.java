48ba6fe5d204994a51e47a11cb1ceb22ca8a5c0e
HSGamer <huynhqtienvtag@gmail.com>
2022-01-14 21:35:42 +0700

    some package info

---
+++ hscore-config/hscore-config-proxy/src/main/java/me/hsgamer/hscore/config/proxy/defaulthandler/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * These classes are used to handle the default methods in interfaces.
+ */
+package me.hsgamer.hscore.config.proxy.defaulthandler;
---
+++ hscore-config/hscore-config-proxy/src/main/java/me/hsgamer/hscore/config/proxy/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains the base classes for the Dynamic Proxy Pattern of {@link me.hsgamer.hscore.config.Config}.
+ */
+package me.hsgamer.hscore.config.proxy;
