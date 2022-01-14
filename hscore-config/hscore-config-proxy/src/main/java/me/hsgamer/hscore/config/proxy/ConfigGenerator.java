df410754e543ca9d55ce6be6318a302e5dba87c8
HSGamer <huynhqtienvtag@gmail.com>
2022-01-14 17:58:32 +0700

    setupConfig

--- hscore-config/hscore-config-proxy/src/main/java/me/hsgamer/hscore/config/proxy/ConfigGenerator.java
+++ hscore-config/hscore-config-proxy/src/main/java/me/hsgamer/hscore/config/proxy/ConfigGenerator.java
@@ -9,12 +9,19 @@ public final class ConfigGenerator {
     // EMPTY
   }
 
-  public static <T> T newInstance(Class<T> clazz, Config config) {
+  public static <T> T newInstance(Class<T> clazz, Config config, boolean setupConfig) {
+    if (setupConfig) {
+      config.setup();
+    }
     Object object = Proxy.newProxyInstance(clazz.getClassLoader(), new Class[]{clazz}, new ConfigInvocationHandler<>(clazz, config));
     if (clazz.isInstance(object)) {
       return clazz.cast(object);
     } else {
       throw new IllegalArgumentException("The class " + clazz.getName() + " is not an instance of " + object.getClass().getName());
     }
+  }
+
+  public static <T> T newInstance(Class<T> clazz, Config config) {
+    return newInstance(clazz, config, true);
   }
 }
