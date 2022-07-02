410127d95deefc7b99e3661746b037d09824db43
HSGamer <huynhqtienvtag@gmail.com>
2022-07-02 23:20:59 +0700

    Use array and loop on updateVersion.sh

--- updateVersion.sh
+++ updateVersion.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+UPDATE_DIR="
+hscore-bukkit
+hscore-config
+hscore-database
+hscore-checker
+hscore-bungeecord
+hscore-minestom
+hscore-downloader
+"
+
+BASE_DIR="$(pwd)"
 mvn -N versions:update-child-modules -DgenerateBackupPoms=false
-cd ../hscore-downloader || return
-mvn -N versions:update-child-modules -DgenerateBackupPoms=false
-cd ./hscore-bukkit || return
-mvn -N versions:update-child-modules -DgenerateBackupPoms=false
-cd ../hscore-config || return
-mvn -N versions:update-child-modules -DgenerateBackupPoms=false
-cd ../hscore-database || return
-mvn -N versions:update-child-modules -DgenerateBackupPoms=false
-cd ../hscore-checker || return
-mvn -N versions:update-child-modules -DgenerateBackupPoms=false
-cd ../hscore-bungeecord || return
-mvn -N versions:update-child-modules -DgenerateBackupPoms=false
-cd ../hscore-minestom || return
-mvn -N versions:update-child-modules -DgenerateBackupPoms=false
+for dir in $UPDATE_DIR; do
+  cd "$BASE_DIR/$dir"
+  mvn -N versions:update-child-modules -DgenerateBackupPoms=false
+done
+cd "$BASE_DIR"
