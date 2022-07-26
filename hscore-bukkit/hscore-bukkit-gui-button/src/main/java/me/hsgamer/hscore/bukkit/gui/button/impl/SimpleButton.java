2766bbe562d91ee9bf818f963a5fbdca74e6c15c
HSGamer <huynhqtienvtag@gmail.com>
2022-07-26 17:05:38 +0700

    less simple SimpleButton

--- hscore-bukkit/hscore-bukkit-gui-button/src/main/java/me/hsgamer/hscore/bukkit/gui/button/impl/SimpleButton.java
+++ hscore-bukkit/hscore-bukkit-gui-button/src/main/java/me/hsgamer/hscore/bukkit/gui/button/impl/SimpleButton.java
@@ -6,13 +6,25 @@ import org.bukkit.inventory.ItemStack;
 
 import java.util.UUID;
 import java.util.function.BiConsumer;
+import java.util.function.Function;
 
 /**
  * A simple button
  */
 public class SimpleButton implements Button {
-  private final ItemStack itemStack;
+  private final Function<UUID, ItemStack> itemStackFunction;
   private final BiConsumer<UUID, InventoryClickEvent> consumer;
+
+  /**
+   * Create a new simple button
+   *
+   * @param itemStackFunction the item stack function
+   * @param consumer          the consumer
+   */
+  public SimpleButton(Function<UUID, ItemStack> itemStackFunction, BiConsumer<UUID, InventoryClickEvent> consumer) {
+    this.itemStackFunction = itemStackFunction;
+    this.consumer = consumer;
+  }
 
   /**
    * Create a new button
@@ -21,13 +33,12 @@ public class SimpleButton implements Button {
    * @param consumer  the consumer
    */
   public SimpleButton(ItemStack itemStack, BiConsumer<UUID, InventoryClickEvent> consumer) {
-    this.itemStack = itemStack;
-    this.consumer = consumer;
+    this(uuid -> itemStack, consumer);
   }
 
   @Override
   public ItemStack getItemStack(UUID uuid) {
-    return itemStack;
+    return itemStackFunction.apply(uuid);
   }
 
   @Override
