69c14e58c524f788466ec8a9e9d36499fb1468a5
HSGamer <huynhqtienvtag@gmail.com>
2022-07-29 10:43:25 +0700

    use json-simple on NBTModifier

--- hscore-bukkit/hscore-bukkit-item/src/main/java/me/hsgamer/hscore/bukkit/item/modifier/NBTModifier.java
+++ hscore-bukkit/hscore-bukkit-item/src/main/java/me/hsgamer/hscore/bukkit/item/modifier/NBTModifier.java
@@ -1,12 +1,12 @@
 package me.hsgamer.hscore.bukkit.item.modifier;
 
-import com.google.gson.Gson;
 import me.hsgamer.hscore.bukkit.item.ItemModifier;
 import me.hsgamer.hscore.common.Validate;
 import me.hsgamer.hscore.common.interfaces.StringReplacer;
 import org.bukkit.Bukkit;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.Contract;
+import org.json.simple.JSONObject;
 
 import java.util.Map;
 import java.util.Objects;
@@ -16,7 +16,6 @@ import java.util.UUID;
  * The NBT Modifier
  */
 public class NBTModifier implements ItemModifier {
-  private static final Gson GSON = new Gson();
   private String nbtData = "";
 
   /**
@@ -59,7 +58,7 @@ public class NBTModifier implements ItemModifier {
   public void loadFromObject(Object object) {
     if (object instanceof Map) {
       Map<?, ?> map = (Map<?, ?>) object;
-      this.nbtData = GSON.toJson(map);
+      this.nbtData = JSONObject.toJSONString(map);
     } else {
       this.nbtData = Objects.toString(object, "");
     }
