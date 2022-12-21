b5956a85129c5620d837237241c38316999049d1
HSGamer <huynhqtienvtag@gmail.com>
2022-12-22 00:05:31 +0700

    docs for VectorIteratorUtil

--- hscore-bukkit/hscore-bukkit-block/src/main/java/me/hsgamer/hscore/bukkit/block/iterator/VectorIteratorUtil.java
+++ hscore-bukkit/hscore-bukkit-block/src/main/java/me/hsgamer/hscore/bukkit/block/iterator/VectorIteratorUtil.java
@@ -7,11 +7,16 @@ import me.hsgamer.hscore.bukkit.block.iterator.impl.RandomVectorIterator;
 import me.hsgamer.hscore.collections.map.CaseInsensitiveStringHashMap;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
 import java.util.function.Function;
 
 import static me.hsgamer.hscore.bukkit.block.iterator.impl.LinearVectorIterator.*;
 
+/**
+ * The utility class for {@link VectorIterator}
+ */
 public final class VectorIteratorUtil {
   private static final Map<String, Function<BlockBox, VectorIterator>> TYPE_MAP = new CaseInsensitiveStringHashMap<>();
 
@@ -30,15 +35,45 @@ public final class VectorIteratorUtil {
     // EMPTY
   }
 
+  /**
+   * Register a {@link VectorIterator} type
+   *
+   * @param type         the type
+   * @param typeFunction the function to create the {@link VectorIterator}
+   */
   public static void registerType(String type, Function<BlockBox, VectorIterator> typeFunction) {
     TYPE_MAP.put(type, typeFunction);
   }
 
+  /**
+   * Create a {@link RandomTypeVectorIterator} for the {@link BlockBox}
+   *
+   * @param box the {@link BlockBox}
+   *
+   * @return the {@link VectorIterator}
+   */
   public static VectorIterator random(BlockBox box) {
     return new RandomTypeVectorIterator(box, new ArrayList<>(TYPE_MAP.values()));
   }
 
+  /**
+   * Get the {@link VectorIterator} by the type
+   *
+   * @param type the type
+   * @param box  the {@link BlockBox}
+   *
+   * @return the {@link VectorIterator}
+   */
   public static VectorIterator get(String type, BlockBox box) {
     return TYPE_MAP.getOrDefault(type, VectorIteratorUtil::random).apply(box);
+  }
+
+  /**
+   * Get the registered types
+   *
+   * @return the registered types
+   */
+  public Collection<String> getRegisteredTypes() {
+    return Collections.unmodifiableSet(TYPE_MAP.keySet());
   }
 }
