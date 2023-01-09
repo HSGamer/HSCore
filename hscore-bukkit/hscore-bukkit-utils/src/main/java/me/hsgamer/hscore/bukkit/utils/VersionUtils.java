52fe72c920675ef149e179d1bef07fbba280ae62
HSGamer <huynhqtienvtag@gmail.com>
2023-01-09 12:23:14 +0700

    minor version and more methods on VersionUtils

--- hscore-bukkit/hscore-bukkit-utils/src/main/java/me/hsgamer/hscore/bukkit/utils/VersionUtils.java
+++ hscore-bukkit/hscore-bukkit-utils/src/main/java/me/hsgamer/hscore/bukkit/utils/VersionUtils.java
@@ -2,6 +2,7 @@ package me.hsgamer.hscore.bukkit.utils;
 
 import org.bukkit.Bukkit;
 
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,14 +10,17 @@ import java.util.regex.Pattern;
  * The helper class for server versions
  */
 public final class VersionUtils {
-  private static final int VERSION;
+  private static final int MAJOR_VERSION;
+  private static final int MINOR_VERSION;
 
   static {
-    Matcher matcher = Pattern.compile("MC: \\d\\.(\\d+)").matcher(Bukkit.getVersion());
+    Matcher matcher = Pattern.compile("MC: \\d\\.(\\d+)(\\.(\\d+))?").matcher(Bukkit.getVersion());
     if (matcher.find()) {
-      VERSION = Integer.parseInt(matcher.group(1));
+      MAJOR_VERSION = Integer.parseInt(matcher.group(1));
+      MINOR_VERSION = Optional.ofNullable(matcher.group(3)).filter(s -> !s.isEmpty()).map(Integer::parseInt).orElse(0);
     } else {
-      VERSION = -1;
+      MAJOR_VERSION = -1;
+      MINOR_VERSION = -1;
     }
   }
 
@@ -30,7 +34,16 @@ public final class VersionUtils {
    * @return the version
    */
   public static int getMajorVersion() {
-    return VERSION;
+    return MAJOR_VERSION;
+  }
+
+  /**
+   * Get the minor version of the server
+   *
+   * @return the version
+   */
+  public static int getMinorVersion() {
+    return MINOR_VERSION;
   }
 
   /**
@@ -41,6 +54,41 @@ public final class VersionUtils {
    * @return true if it is
    */
   public static boolean isAtLeast(int version) {
-    return VERSION >= version;
+    return MAJOR_VERSION >= version;
+  }
+
+  /**
+   * Check if the server version is at least the given version
+   *
+   * @param majorVersion the major version to check
+   * @param minorVersion the minor version to check
+   *
+   * @return true if it is
+   */
+  public static boolean isAtLeast(int majorVersion, int minorVersion) {
+    return MAJOR_VERSION > majorVersion || (MAJOR_VERSION == majorVersion && MINOR_VERSION >= minorVersion);
+  }
+
+  /**
+   * Check if the server version is at the given version
+   *
+   * @param majorVersion the major version to check
+   *
+   * @return true if it is
+   */
+  public static boolean isAt(int majorVersion) {
+    return MAJOR_VERSION == majorVersion;
+  }
+
+  /**
+   * Check if the server version is at the given version
+   *
+   * @param majorVersion the major version to check
+   * @param minorVersion the minor version to check
+   *
+   * @return true if it is
+   */
+  public static boolean isAt(int majorVersion, int minorVersion) {
+    return MAJOR_VERSION == majorVersion && MINOR_VERSION == minorVersion;
   }
 }
