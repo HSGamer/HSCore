9a290af23ed2a7e561ed9d5250e1424e58a9657b
HSGamer <huynhqtienvtag@gmail.com>
2023-01-20 21:04:04 +0700

    registerPermission

--- hscore-bukkit/hscore-bukkit-baseplugin/src/main/java/me/hsgamer/hscore/bukkit/baseplugin/BasePlugin.java
+++ hscore-bukkit/hscore-bukkit-baseplugin/src/main/java/me/hsgamer/hscore/bukkit/baseplugin/BasePlugin.java
@@ -4,16 +4,24 @@ import me.hsgamer.hscore.bukkit.command.CommandManager;
 import me.hsgamer.hscore.bukkit.simpleplugin.SimplePlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
+import org.bukkit.permissions.Permission;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPluginLoader;
 
 import java.io.File;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.util.logging.Level.WARNING;
 
 /**
  * A slightly complicated {@link SimplePlugin} implementation
  */
 public class BasePlugin extends SimplePlugin {
   private final CommandManager commandManager = new CommandManager(this);
+  private final Set<Permission> registeredPermissions = new HashSet<>();
 
   public BasePlugin() {
     super();
@@ -31,6 +39,7 @@ public class BasePlugin extends SimplePlugin {
   @Override
   public final void onEnable() {
     super.onEnable();
+    registerPermissions();
     Bukkit.getScheduler().scheduleSyncDelayedTask(this, CommandManager::syncCommand);
   }
 
@@ -38,6 +47,45 @@ public class BasePlugin extends SimplePlugin {
   public final void onDisable() {
     super.onDisable();
     commandManager.unregisterAll();
+    registeredPermissions.forEach(Bukkit.getPluginManager()::removePermission);
+  }
+
+  private void registerPermissions() {
+    Class<?> clazz = getPermissionClass();
+    if (clazz != null) {
+      for (Field field : clazz.getDeclaredFields()) {
+        int modifiers = field.getModifiers();
+        if (!field.getType().equals(Permission.class) || !Modifier.isStatic(modifiers) || !Modifier.isPublic(modifiers))
+          continue;
+
+        try {
+          Permission permission = (Permission) field.get(null);
+          registerPermission(permission);
+        } catch (IllegalAccessException e) {
+          getLogger().log(WARNING, "Failed to register permission", e);
+        }
+      }
+    }
+  }
+
+  /**
+   * Get the class containing all the {@link Permission}s.
+   * The permissions must be static, public, and {@link Permission} type.
+   *
+   * @return the class
+   */
+  protected Class<?> getPermissionClass() {
+    return getClass();
+  }
+
+  /**
+   * Register a {@link Permission}
+   *
+   * @param permission the permission
+   */
+  public void registerPermission(Permission permission) {
+    Bukkit.getPluginManager().addPermission(permission);
+    registeredPermissions.add(permission);
   }
 
   /**
