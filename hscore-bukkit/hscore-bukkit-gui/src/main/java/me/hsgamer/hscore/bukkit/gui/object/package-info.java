ef0e9ceca70cf59b6893e1d6656f95d80951f352
HSGamer <huynhqtienvtag@gmail.com>
2023-01-21 17:35:18 +0700

    fix doc ref

--- hscore-bukkit/hscore-bukkit-gui/src/main/java/me/hsgamer/hscore/bukkit/gui/object/package-info.java
+++ hscore-bukkit/hscore-bukkit-gui/src/main/java/me/hsgamer/hscore/bukkit/gui/object/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Contains the Bukkit implementation of {@link me.hsgamer.hscore.minestom.gui.object}
+ * Contains the Bukkit implementation of {@link me.hsgamer.hscore.minecraft.gui.object}
  */
 package me.hsgamer.hscore.bukkit.gui.object;
--- hscore-minestom/hscore-minestom-gui/src/main/java/me/hsgamer/hscore/minestom/gui/object/package-info.java
+++ hscore-minestom/hscore-minestom-gui/src/main/java/me/hsgamer/hscore/minestom/gui/object/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Contains the Minestom implementation of {@link me.hsgamer.hscore.minestom.gui.object}
+ * Contains the Minestom implementation of {@link me.hsgamer.hscore.minecraft.gui.object}
  */
 package me.hsgamer.hscore.minestom.gui.object;
