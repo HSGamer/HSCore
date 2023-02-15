81a084512404344a7c3e40924057631244208734
HSGamer <huynhqtienvtag@gmail.com>
2023-02-15 11:25:49 +0700

    default ExpansionFactory

--- hscore-expansion/hscore-expansion-common/src/main/java/me/hsgamer/hscore/expansion/common/ExpansionFactory.java
+++ hscore-expansion/hscore-expansion-common/src/main/java/me/hsgamer/hscore/expansion/common/ExpansionFactory.java
@@ -1,6 +1,21 @@
 package me.hsgamer.hscore.expansion.common;
 
 public interface ExpansionFactory {
+  ExpansionFactory DEFAULT = classLoader -> {
+    try {
+      final Class<?> clazz = Class.forName(classLoader.getAddonDescription().getMainClass(), true, classLoader);
+      final Class<? extends Expansion> newClass;
+      if (Expansion.class.isAssignableFrom(clazz)) {
+        newClass = clazz.asSubclass(Expansion.class);
+      } else {
+        throw new ClassCastException("The main class does not extend " + Expansion.class.getSimpleName());
+      }
+      return newClass.getDeclaredConstructor().newInstance();
+    } catch (Exception e) {
+      throw new InvalidExpansionException("Can't load the main class", e);
+    }
+  };
+
   /**
    * Create an expansion from the class loader
    */
