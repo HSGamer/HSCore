1a0a1babc125aa8853c111094ec62d87fb843915
HSGamer <huynhqtienvtag@gmail.com>
2023-04-27 19:25:45 +0700

    cache the logger in LoggerProvider

--- hscore-logger/hscore-logger-provider/src/main/java/me/hsgamer/hscore/logger/provider/LoggerProvider.java
+++ hscore-logger/hscore-logger-provider/src/main/java/me/hsgamer/hscore/logger/provider/LoggerProvider.java
@@ -4,9 +4,12 @@ import me.hsgamer.hscore.logger.common.LogLevel;
 import me.hsgamer.hscore.logger.common.Logger;
 import me.hsgamer.hscore.logger.jul.JulLogger;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 
 public class LoggerProvider {
+  private static final Map<String, Logger> LOGGER_MAP = new ConcurrentHashMap<>();
   private static Function<String, Logger> LOGGER_PROVIDER = JulLogger::new;
   private static boolean urgentLoad = false;
 
@@ -40,21 +43,23 @@ public class LoggerProvider {
    * @return the logger
    */
   public static Logger getLogger(String name) {
-    if (urgentLoad) {
-      return LOGGER_PROVIDER.apply(name);
-    } else {
-      return new Logger() {
-        private Logger logger;
+    return LOGGER_MAP.computeIfAbsent(name, s -> {
+      if (urgentLoad) {
+        return LOGGER_PROVIDER.apply(s);
+      } else {
+        return new Logger() {
+          private Logger logger;
 
-        @Override
-        public void log(LogLevel level, String message) {
-          if (logger == null) {
-            logger = LOGGER_PROVIDER.apply(name);
+          @Override
+          public void log(LogLevel level, String message) {
+            if (logger == null) {
+              logger = LOGGER_PROVIDER.apply(s);
+            }
+            logger.log(level, message);
           }
-          logger.log(level, message);
-        }
-      };
-    }
+        };
+      }
+    });
   }
 
   /**
