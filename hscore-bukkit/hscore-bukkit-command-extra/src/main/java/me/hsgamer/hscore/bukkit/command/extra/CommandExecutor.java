660dca018adcc48d20dd13478f29f7d572bbfe4a
HSGamer <huynhqtienvtag@gmail.com>
2023-04-29 11:59:09 +0700

    remove bukkit-command-extra

--- hscore-bukkit/hscore-bukkit-command-extra/src/main/java/me/hsgamer/hscore/bukkit/command/extra/CommandExecutor.java
+++
@@ -1,7 +0,0 @@
-package me.hsgamer.hscore.bukkit.command.extra;
-
-import org.bukkit.command.CommandSender;
-
-public interface CommandExecutor {
-  boolean execute(CommandSender sender, String label, String... args);
-}
