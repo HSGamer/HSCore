6fce2d8978107524476820cdeec33db2eecec8c1
HSGamer <huynhqtienvtag@gmail.com>
2023-04-29 15:59:58 +0700

    release

--- updateVersion.sh
+++ updateVersion.sh
@@ -7,7 +7,6 @@ hscore-checker
 hscore-bungeecord
 hscore-minestom
 hscore-downloader
-hscore-logging
 hscore-minecraft
 hscore-expansion
 hscore-logger
@@ -16,7 +15,7 @@ hscore-logger
 BASE_DIR="$(pwd)"
 mvn -N versions:update-child-modules -DgenerateBackupPoms=false
 for dir in $UPDATE_DIR; do
-  cd "$BASE_DIR/$dir"
+  cd "$BASE_DIR/$dir" || exit
   mvn -N versions:update-child-modules -DgenerateBackupPoms=false
 done
-cd "$BASE_DIR"
+cd "$BASE_DIR" || exit
