bf91dfe63de7350f408a9fd3f5759c2d9f1f3de3
HSGamer <huynhqtienvtag@gmail.com>
2023-05-07 08:38:38 +0700

    simplify Board to just one method

--- hscore-minestom/hscore-minestom-board/src/main/java/me/hsgamer/hscore/minestom/board/Board.java
+++ hscore-minestom/hscore-minestom-board/src/main/java/me/hsgamer/hscore/minestom/board/Board.java
@@ -8,8 +8,10 @@ import net.minestom.server.event.EventNode;
 import net.minestom.server.event.player.PlayerDisconnectEvent;
 import net.minestom.server.scoreboard.Sidebar;
 
-import java.util.*;
-import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 
 /**
@@ -17,10 +19,9 @@ import java.util.function.Function;
  * Call {@link #hook(EventNode)} when the server is starting up to make sure the feature work properly.
  */
 public class Board {
-  private static final Map<Player, Sidebar> boards = new HashMap<>();
+  private static final Map<Player, Sidebar> boards = new ConcurrentHashMap<>();
   private static final int MAX_LINES = 15;
   private static final String MAGIC_STRING = "boardLine_";
-  private final Collection<Player> players = new ConcurrentLinkedDeque<>();
   private final Function<Player, Component> title;
   private final Function<Player, List<Component>> lines;
 
@@ -45,76 +46,24 @@ public class Board {
     node.addListener(PlayerDisconnectEvent.class, event -> Board.removeBoard(event.getPlayer()));
   }
 
-  private static void initBoard(Player player) {
-    if (boards.containsKey(player)) {
-      return;
-    }
-    Sidebar sidebar = new Sidebar(Component.text(""));
-    sidebar.addViewer(player);
-    boards.put(player, sidebar);
+  private static Sidebar getBoard(Player player) {
+    return boards.computeIfAbsent(player, p -> {
+      Sidebar sidebar = new Sidebar(Component.text(""));
+      sidebar.addViewer(p);
+      return sidebar;
+    });
   }
 
   private static void removeBoard(Player player) {
     Optional.ofNullable(boards.remove(player)).ifPresent(sidebar -> sidebar.removeViewer(player));
   }
 
-  /**
-   * Add the player to the board
-   *
-   * @param player the player
-   */
-  public void addPlayer(Player player) {
-    initBoard(player);
-    players.add(player);
-    update(player);
-  }
+  private static void updateBoard(Player player, Component title, List<Component> lines) {
+    Sidebar sidebar = getBoard(player);
 
-  /**
-   * Remove the player from the board
-   *
-   * @param player the player
-   */
-  public void removePlayer(Player player) {
-    players.remove(player);
-  }
+    sidebar.setTitle(title);
 
-  /**
-   * Remove all the players from the board
-   */
-  public void removeAll() {
-    players.clear();
-  }
-
-  /**
-   * Update the board for the player
-   *
-   * @param player the player
-   *
-   * @return true if the board was updated, false if the player is not in the board
-   */
-  public boolean update(Player player) {
-    Sidebar sidebar = boards.get(player);
-    if (sidebar == null) {
-      return false;
-    }
-
-    updateBoard(player, sidebar);
-    return true;
-  }
-
-  /**
-   * Update the board for all the players
-   */
-  public void updateAll() {
-    for (Player player : players) {
-      update(player);
-    }
-  }
-
-  private void updateBoard(Player player, Sidebar sidebar) {
-    sidebar.setTitle(title.apply(player));
-
-    List<Component> lineList = CollectionUtils.reverse(lines.apply(player));
+    List<Component> lineList = CollectionUtils.reverse(lines);
     for (int i = 0; i < MAX_LINES; i++) {
       String lineId = MAGIC_STRING + i;
       Sidebar.ScoreboardLine line = sidebar.getLine(lineId);
@@ -129,5 +78,14 @@ public class Board {
         sidebar.updateLineContent(lineId, component);
       }
     }
+  }
+
+  /**
+   * Update the board for the player
+   *
+   * @param player the player
+   */
+  public void update(Player player) {
+    updateBoard(player, title.apply(player), lines.apply(player));
   }
 }
