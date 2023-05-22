345df8518224619e30c027d62b2265a0530f56c1
HSGamer <huynhqtienvtag@gmail.com>
2023-05-22 23:59:29 +0700

    add VariableBundle & bukkit-variable

---
+++ hscore-bukkit/hscore-bukkit-variable/src/main/java/me.hsgamer.hscore.bukkit.variable/BukkitVariableBundle.java
@@ -0,0 +1,120 @@
+package me.hsgamer.hscore.bukkit.variable;
+
+import me.hsgamer.hscore.bukkit.utils.BukkitUtils;
+import me.hsgamer.hscore.common.StringReplacer;
+import me.hsgamer.hscore.common.Validate;
+import me.hsgamer.hscore.variable.VariableBundle;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+import java.util.Optional;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * The utility class to manage the global {@link VariableBundle} for Bukkit
+ */
+public final class BukkitVariableBundle {
+  /**
+   * The global {@link VariableBundle} for Bukkit
+   */
+  public static final VariableBundle BUNDLE = new VariableBundle();
+
+  private BukkitVariableBundle() {
+    // EMPTY
+  }
+
+  /**
+   * Register the default variables to {@link BukkitVariableBundle#BUNDLE}
+   */
+  public static void registerDefaultVariables() {
+    // Player Name
+    BUNDLE.register("player", StringReplacer.of((original, uuid) -> Bukkit.getOfflinePlayer(uuid).getName()), true);
+
+    // Online Player
+    BUNDLE.register("online", StringReplacer.of(original -> String.valueOf(Bukkit.getOnlinePlayers().size())), true);
+
+    // Max Players
+    BUNDLE.register("max_players", StringReplacer.of(original -> String.valueOf(Bukkit.getMaxPlayers())), true);
+
+    // Location
+    BUNDLE.register("world", StringReplacer.of((original, uuid) -> {
+      Optional<World> optional = Optional.ofNullable(Bukkit.getPlayer(uuid)).map(player -> player.getLocation().getWorld());
+      if (original.equalsIgnoreCase("_env")) {
+        return optional.map(World::getEnvironment).map(Enum::name).orElse("");
+      } else if (original.isEmpty()) {
+        return optional.map(World::getName).orElse("");
+      } else {
+        return null;
+      }
+    }));
+    BUNDLE.register("x", StringReplacer.of((original, uuid) -> Optional.ofNullable(Bukkit.getPlayer(uuid)).map(Player::getLocation).map(Location::getX).map(String::valueOf).orElse("")), true);
+    BUNDLE.register("y", StringReplacer.of((original, uuid) -> Optional.ofNullable(Bukkit.getPlayer(uuid)).map(Player::getLocation).map(Location::getY).map(String::valueOf).orElse("")), true);
+    BUNDLE.register("z", StringReplacer.of((original, uuid) -> Optional.ofNullable(Bukkit.getPlayer(uuid)).map(Player::getLocation).map(Location::getZ).map(String::valueOf).orElse("")), true);
+
+    // Bed Location
+    BUNDLE.register("bed_", StringReplacer.of((original, uuid) -> {
+      Player player = Bukkit.getPlayer(uuid);
+      if (player == null || player.getBedSpawnLocation() == null) {
+        return "";
+      } else if (original.equalsIgnoreCase("world")) {
+        return player.getBedSpawnLocation().getWorld().getName();
+      } else if (original.equalsIgnoreCase("x")) {
+        return String.valueOf(player.getBedSpawnLocation().getX());
+      } else if (original.equalsIgnoreCase("y")) {
+        return String.valueOf(player.getBedSpawnLocation().getY());
+      } else if (original.equalsIgnoreCase("z")) {
+        return String.valueOf(player.getBedSpawnLocation().getZ());
+      } else if (original.equalsIgnoreCase("world_env")) {
+        return player.getBedSpawnLocation().getWorld().getEnvironment().name();
+      } else {
+        return null;
+      }
+    }));
+
+    // Exp
+    BUNDLE.register("exp", StringReplacer.of((original, uuid) -> Optional.ofNullable(Bukkit.getPlayer(uuid)).map(Player::getExp).map(String::valueOf).orElse("")), true);
+
+    // Level
+    BUNDLE.register("level", StringReplacer.of((original, uuid) -> Optional.ofNullable(Bukkit.getPlayer(uuid)).map(Player::getLevel).map(String::valueOf).orElse("")), true);
+
+    // Exp to level
+    BUNDLE.register("exp_to_level", StringReplacer.of((original, uuid) -> Optional.ofNullable(Bukkit.getPlayer(uuid)).map(Player::getExpToLevel).map(String::valueOf).orElse("")), true);
+
+    // Food Level
+    BUNDLE.register("food_level", StringReplacer.of((original, uuid) -> Optional.ofNullable(Bukkit.getPlayer(uuid)).map(Player::getFoodLevel).map(String::valueOf).orElse("")), true);
+
+    // IP
+    BUNDLE.register("ip", StringReplacer.of((original, uuid) -> Optional.ofNullable(Bukkit.getPlayer(uuid)).map(Player::getAddress).map(address -> address.getAddress().getHostName()).orElse("")), true);
+
+    // Biome
+    BUNDLE.register("biome", StringReplacer.of((original, uuid) -> Optional.ofNullable(Bukkit.getPlayer(uuid)).map(Player::getLocation).map(Location::getBlock).map(block -> block.getBiome().name()).orElse("")), true);
+
+    // Ping
+    BUNDLE.register("ping", StringReplacer.of((original, uuid) -> Optional.ofNullable(Bukkit.getPlayer(uuid)).map(BukkitUtils::getPing).map(String::valueOf).orElse("")), true);
+
+    // Random
+    BUNDLE.register("random_", StringReplacer.of((original, uuid) -> {
+      original = original.trim();
+      if (original.contains(":")) {
+        String[] split = original.split(":", 2);
+        String s1 = split[0].trim();
+        String s2 = split[1].trim();
+        if (Validate.isValidInteger(s1) && Validate.isValidInteger(s2)) {
+          int i1 = Integer.parseInt(s1);
+          int i2 = Integer.parseInt(s2);
+          int max = Math.max(i1, i2);
+          int min = Math.min(i1, i2);
+          return String.valueOf(ThreadLocalRandom.current().nextInt(min, max + 1));
+        }
+      } else if (Validate.isValidInteger(original)) {
+        return String.valueOf(ThreadLocalRandom.current().nextInt(Integer.parseInt(original)));
+      }
+      return null;
+    }));
+
+    // UUID
+    BUNDLE.register("uuid", StringReplacer.of((original, uuid) -> uuid.toString()), true);
+  }
+}
---
+++ hscore-variable/src/main/java/me/hsgamer/hscore/variable/VariableBundle.java
@@ -0,0 +1,82 @@
+package me.hsgamer.hscore.variable;
+
+import me.hsgamer.hscore.common.StringReplacer;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * A bundle of variables that is useful for grouping variables
+ */
+public class VariableBundle {
+  private final Set<String> variablePrefix = new HashSet<>();
+  private final VariableManager variableManager;
+
+  /**
+   * Create a new bundle for the variable manager
+   *
+   * @param variableManager the variable manager
+   */
+  public VariableBundle(VariableManager variableManager) {
+    this.variableManager = variableManager;
+  }
+
+  /**
+   * Create a new bundle for the global variable manager
+   */
+  public VariableBundle() {
+    this(VariableManager.GLOBAL);
+  }
+
+  /**
+   * Register new variable
+   *
+   * @param prefix   the prefix
+   * @param variable the replacer
+   * @param isWhole  whether the manager should check the whole string matches the prefix, set it to false if you want to check if the prefix is at the beginning of the string
+   *
+   * @return true if the registration is successful or false if the prefix already exists
+   */
+  public boolean register(String prefix, StringReplacer variable, boolean isWhole) {
+    if (variablePrefix.add(prefix)) {
+      variableManager.register(prefix, variable, isWhole);
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Register new variable
+   *
+   * @param prefix   the prefix
+   * @param variable the Variable object
+   *
+   * @return true if the registration is successful or false if the prefix already exists
+   */
+  public boolean register(String prefix, StringReplacer variable) {
+    return register(prefix, variable, false);
+  }
+
+  /**
+   * Unregister a variable
+   *
+   * @param prefix the prefix
+   *
+   * @return true if the un-registration is successful or false if the prefix doesn't exist
+   */
+  public boolean unregister(String prefix) {
+    if (variablePrefix.remove(prefix)) {
+      variableManager.unregister(prefix);
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Unregister all variables
+   */
+  public void unregisterAll() {
+    variablePrefix.forEach(variableManager::unregister);
+    variablePrefix.clear();
+  }
+}
