1e6f64c76c46833fc39861d0acd658164c474b98
hsgamer <huynhqtienvtag@gmail.com>
2023-11-23 22:25:00 +0700

    update HibernateClient to use its Configuration

--- hscore-database/hscore-database-client-hibernate/src/main/java/me/hsgamer/hscore/database/client/hibernate/HibernateClient.java
+++ hscore-database/hscore-database-client-hibernate/src/main/java/me/hsgamer/hscore/database/client/hibernate/HibernateClient.java
@@ -4,20 +4,16 @@ import me.hsgamer.hscore.database.BaseClient;
 import me.hsgamer.hscore.database.Driver;
 import me.hsgamer.hscore.database.Setting;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.AvailableSettings;
-import org.hibernate.service.ServiceRegistry;
+import org.hibernate.cfg.Configuration;
 
 import java.util.function.Consumer;
 
 /**
  * The Hibernate client
  */
-public class HibernateClient extends BaseClient<MetadataSources> {
-  private final MetadataSources metadataSources;
-  private Metadata metadata;
+public class HibernateClient extends BaseClient<Configuration> {
+  private final Configuration configuration;
 
   /**
    * Create new Hibernate client
@@ -27,55 +23,49 @@ public class HibernateClient extends BaseClient<MetadataSources> {
   public HibernateClient(Setting setting) {
     super(setting);
     Driver driver = setting.getDriver();
-    ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-      .applySetting(AvailableSettings.JAKARTA_JDBC_DRIVER, driver.getDriverClass().getName())
-      .applySetting(AvailableSettings.JAKARTA_JDBC_URL, driver.convertURL(setting))
-      .applySetting(AvailableSettings.JAKARTA_JDBC_USER, setting.getUsername())
-      .applySetting(AvailableSettings.JAKARTA_JDBC_PASSWORD, setting.getPassword())
-      .applySettings(setting.getClientProperties())
-      .build();
-    this.metadataSources = new MetadataSources(serviceRegistry);
+
+    // TODO: Use JPA-standard properties when the weird error is fixed
+    //noinspection deprecation
+    this.configuration = new Configuration()
+      .setProperty(AvailableSettings.URL, driver.convertURL(setting))
+      .setProperty(AvailableSettings.DRIVER, driver.getDriverClass().getName())
+      .setProperty(AvailableSettings.USER, setting.getUsername())
+      .setProperty(AvailableSettings.PASS, setting.getPassword());
+
+    setting.getClientProperties().forEach((key, value) -> configuration.setProperty(key, value.toString()));
   }
 
   @Override
-  public MetadataSources getOriginal() {
-    return metadataSources;
+  public Configuration getOriginal() {
+    return configuration;
   }
 
   /**
-   * Configure the metadata sources
+   * Configure the configuration
    *
-   * @param metadataSourcesConsumer the consumer for the metadata sources
+   * @param configurationConsumer the consumer
    *
    * @return the client for chaining
    */
-  public HibernateClient configure(Consumer<MetadataSources> metadataSourcesConsumer) {
-    metadataSourcesConsumer.accept(metadataSources);
+  public HibernateClient configure(Consumer<Configuration> configurationConsumer) {
+    configurationConsumer.accept(configuration);
     return this;
   }
 
   /**
-   * Add an entity class to the client
+   * Add entity class to the client
    *
    * @param clazz the entity class
    *
    * @return the client for chaining
-   */
-  public HibernateClient addEntityClass(Class<?> clazz) {
-    metadataSources.addAnnotatedClass(clazz);
-    return this;
-  }
-
-  /**
-   * Get the metadata of the client
    *
-   * @return the metadata
+   * @see Configuration#addAnnotatedClass(Class)
    */
-  public Metadata getMetadata() {
-    if (metadata == null) {
-      metadata = metadataSources.buildMetadata();
+  public HibernateClient addEntityClass(Class<?>... clazz) {
+    for (Class<?> c : clazz) {
+      configuration.addAnnotatedClass(c);
     }
-    return metadata;
+    return this;
   }
 
   /**
@@ -84,6 +74,6 @@ public class HibernateClient extends BaseClient<MetadataSources> {
    * @return the session factory
    */
   public SessionFactory buildSessionFactory() {
-    return getMetadata().buildSessionFactory();
+    return configuration.buildSessionFactory();
   }
 }
