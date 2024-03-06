30b13222c09c65bb1f3d8eb2427bed82928ddde6
HSGamer <huynhqtienvtag@gmail.com>
2024-03-06 23:25:20 +0700

    fix CommonVariableBundle

--- hscore-variable/src/main/java/me/hsgamer/hscore/variable/CommonVariableBundle.java
+++ hscore-variable/src/main/java/me/hsgamer/hscore/variable/CommonVariableBundle.java
@@ -3,6 +3,7 @@ package me.hsgamer.hscore.variable;
 import me.hsgamer.hscore.common.StringReplacer;
 import me.hsgamer.hscore.common.Validate;
 
+import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
@@ -26,15 +27,20 @@ public final class CommonVariableBundle {
         String[] split = original.split(":", 2);
         String s1 = split[0].trim();
         String s2 = split[1].trim();
-        if (Validate.isValidInteger(s1) && Validate.isValidInteger(s2)) {
-          int i1 = Integer.parseInt(s1);
-          int i2 = Integer.parseInt(s2);
+        Optional<Integer> optional1 = Validate.getNumber(s1).map(Number::intValue);
+        Optional<Integer> optional2 = Validate.getNumber(s2).map(Number::intValue);
+        if (optional1.isPresent() && optional2.isPresent()) {
+          int i1 = optional1.get();
+          int i2 = optional2.get();
           int max = Math.max(i1, i2);
           int min = Math.min(i1, i2);
           return String.valueOf(ThreadLocalRandom.current().nextInt(min, max + 1));
         }
-      } else if (Validate.isValidInteger(original)) {
-        return String.valueOf(ThreadLocalRandom.current().nextInt(Integer.parseInt(original)));
+      } else {
+        Optional<Integer> optional = Validate.getNumber(original).map(Number::intValue);
+        if (optional.isPresent()) {
+          return String.valueOf(ThreadLocalRandom.current().nextInt(optional.get()));
+        }
       }
       return null;
     }));
