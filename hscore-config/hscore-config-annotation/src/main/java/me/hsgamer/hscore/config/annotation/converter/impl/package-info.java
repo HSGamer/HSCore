8ca38b3e0faef70683b04f798b29f3dd05176331
HSGamer <huynhqtienvtag@gmail.com>
2024-03-06 22:33:46 +0700

    correct package comment for converter impl

--- hscore-config/hscore-config-annotation/src/main/java/me/hsgamer/hscore/config/annotation/converter/impl/package-info.java
+++ hscore-config/hscore-config-annotation/src/main/java/me/hsgamer/hscore/config/annotation/converter/impl/package-info.java
@@ -1,4 +1,4 @@
 /**
- * The implementation for the converter
+ * Contains the implementation for {@link me.hsgamer.hscore.config.annotation.converter.Converter} and {@link me.hsgamer.hscore.config.annotation.converter.ConverterProvider}
  */
 package me.hsgamer.hscore.config.annotation.converter.impl;
