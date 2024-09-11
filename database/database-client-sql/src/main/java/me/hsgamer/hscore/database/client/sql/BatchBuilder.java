1f7ff76f1871b5a693aa68bde87dd00bb24115e2
hsgamer <huynhqtienvtag@gmail.com>
2024-09-11 10:41:37 +0700

    add BatchBuilder

---
+++ database/database-client-sql/src/main/java/me/hsgamer/hscore/database/client/sql/BatchBuilder.java
@@ -0,0 +1,89 @@
+package me.hsgamer.hscore.database.client.sql;
+
+import me.hsgamer.hscore.logger.common.LogLevel;
+import me.hsgamer.hscore.logger.common.Logger;
+import me.hsgamer.hscore.logger.provider.LoggerProvider;
+import org.intellij.lang.annotations.Language;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A builder for batch execution
+ */
+public class BatchBuilder {
+  private static final Logger LOGGER = LoggerProvider.getLogger(BatchBuilder.class);
+  private final Connection connection;
+  private final @Language("SQL") String statement;
+  private final List<Object[]> values = new ArrayList<>();
+
+  /**
+   * Create a new builder
+   *
+   * @param connection the connection
+   * @param statement  the statement
+   */
+  public BatchBuilder(Connection connection, @Language("SQL") String statement) {
+    this.connection = connection;
+    this.statement = statement;
+  }
+
+  /**
+   * Add values to the batch
+   *
+   * @param values the values
+   *
+   * @return this builder for chaining
+   */
+  public BatchBuilder addValues(Object... values) {
+    this.values.add(values);
+    return this;
+  }
+
+  /**
+   * Add values to the batch
+   *
+   * @param values the values
+   *
+   * @return this builder for chaining
+   */
+  public BatchBuilder addValues(List<Object> values) {
+    return addValues(values.toArray());
+  }
+
+  /**
+   * Execute the batch
+   *
+   * @return the result of the batch
+   *
+   * @throws SQLException if a SQL error occurs
+   */
+  public int[] execute() throws SQLException {
+    try (PreparedStatement preparedStatement = connection.prepareStatement(statement)) {
+      for (Object[] value : values) {
+        for (int i = 0; i < value.length; i++) {
+          preparedStatement.setObject(i + 1, value[i]);
+        }
+        preparedStatement.addBatch();
+      }
+      return preparedStatement.executeBatch();
+    }
+  }
+
+  /**
+   * Execute the batch but ignore the exception
+   *
+   * @return the result of the batch
+   */
+  public int[] executeSafe() {
+    try {
+      return execute();
+    } catch (SQLException e) {
+      LOGGER.log(LogLevel.WARN, e);
+      return new int[0];
+    }
+  }
+}
--- database/database-client-sql/src/main/java/me/hsgamer/hscore/database/client/sql/SqlClient.java
+++ database/database-client-sql/src/main/java/me/hsgamer/hscore/database/client/sql/SqlClient.java
@@ -3,6 +3,7 @@ package me.hsgamer.hscore.database.client.sql;
 import me.hsgamer.hscore.database.Client;
 import me.hsgamer.hscore.logger.common.LogLevel;
 import me.hsgamer.hscore.logger.provider.LoggerProvider;
+import org.intellij.lang.annotations.Language;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -43,6 +44,35 @@ public interface SqlClient<T> extends Client<T> {
   default Optional<StatementBuilder> createStatementBuilderSafe() {
     try {
       return Optional.of(this.createStatementBuilder());
+    } catch (Exception e) {
+      LoggerProvider.getLogger(this.getClass()).log(LogLevel.WARN, e);
+      return Optional.empty();
+    }
+  }
+
+  /**
+   * Create a new batch builder for this client
+   *
+   * @param statement the statement
+   *
+   * @return the batch builder
+   *
+   * @throws SQLException if there is an SQL error
+   */
+  default BatchBuilder createBatchBuilder(@Language("SQL") String statement) throws SQLException {
+    return new BatchBuilder(this.getConnection(), statement);
+  }
+
+  /**
+   * Create a new batch builder for this client but ignores exceptions
+   *
+   * @param statement the statement
+   *
+   * @return the batch builder
+   */
+  default Optional<BatchBuilder> createBatchBuilderSafe(@Language("SQL") String statement) {
+    try {
+      return Optional.of(this.createBatchBuilder(statement));
     } catch (Exception e) {
       LoggerProvider.getLogger(this.getClass()).log(LogLevel.WARN, e);
       return Optional.empty();
